6b11ea1e2f55613f7fe906949edcfbcd904a3fe0
tasinvoid <shoaibmahmud4000@gmail.com>
2025-06-09 16:13:17 +0600

    first commit

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router";
+import NavBar from "../pages/Shared/NavBar";
+import Footer from "../pages/Shared/Footer";
 
 const RootLayout = () => {
   return (
     <div>
+      <NavBar></NavBar>
       <Outlet></Outlet>
+      <Footer></Footer>
     </div>
   );
 };
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router";
+import Home from "../pages/Home/Home";
+import RootLayout from "../layouts/RootLayout";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>,
+    Component: RootLayout,
+    children: [
+      {
+        index: true,
+        Component: Home,
+      },
+    ],
   },
 ]);
 
